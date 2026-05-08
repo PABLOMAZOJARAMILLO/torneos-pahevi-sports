f62bd5e801a76a71cf101688a6e743e4fb6896e1
Pablo Cesar Mazo <PABLOMAZO1709@HOTMAIL.COM>
2026-05-08 02:59:59 -0500

    Corregir csrf importador admin

--- torneos_imcred/settings.py
+++ torneos_imcred/settings.py
@@ -134,3 +134,6 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CSRF_TRUSTED_ORIGINS = [
+    "https://torneos-imcred.onrender.com",
+]
