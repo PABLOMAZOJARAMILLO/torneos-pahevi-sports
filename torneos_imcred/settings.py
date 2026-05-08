7c825510fc79b32dd0d55fd668e7a16552d611ed
Pablo Cesar Mazo <PABLOMAZO1709@HOTMAIL.COM>
2026-05-08 01:48:12 -0500

    Agregar importador de planilla oficial

--- torneos_imcred/settings.py
+++ torneos_imcred/settings.py
@@ -121,7 +121,14 @@ STATIC_URL = '/static/'
 
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+    },
+}
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
