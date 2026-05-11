e8b68212d982891a4902f7dd09d539af7d99afd0
Pablo Cesar Mazo <PABLOMAZO1709@HOTMAIL.COM>
2026-05-11 16:42:48 -0500

    Register torneo model in admin

--- torneos/admin.py
+++ torneos/admin.py
@@ -8,6 +8,14 @@ from import_export.widgets import ForeignKeyWidget
 from openpyxl import load_workbook
 
 from .models import Categoria, Equipo, Jugador, Partido, Gol, Tarjeta, AlineacionPartido, SustitucionPartido
+from django.contrib import admin
+from .models import Torneo, Categoria, Equipo, Jugador, Partido
+
+admin.site.register(Torneo)
+admin.site.register(Categoria)
+admin.site.register(Equipo)
+admin.site.register(Jugador)
+admin.site.register(Partido)
 
 
 def limpiar_texto(valor):
