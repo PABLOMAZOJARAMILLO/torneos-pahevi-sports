198998e0226adc7f277aad6b3aa93f4fb9c809bc
Pablo Cesar Mazo <PABLOMAZO1709@HOTMAIL.COM>
2026-05-18 18:35:09 -0500

    Ampliar limpieza de nombres de jugadores

--- torneos/templatetags/texto_limpio.py
+++ torneos/templatetags/texto_limpio.py
@@ -3,15 +3,28 @@ from django import template
 register = template.Library()
 
 
-MOJIBAKE_DOS = str.maketrans({
-    "┴": "Á",
-    "╡": "Á",
-    "┐": "Á",
-    "╔": "É",
-    "╓": "Í",
-    "═": "Í",
-    "α": "Ó",
-    "Θ": "Ú",
+def _tabla_cp437_a_cp850():
+    tabla = {}
+    for codigo in range(128, 256):
+        malo = bytes([codigo]).decode("cp437")
+        bueno = bytes([codigo]).decode("cp850")
+        if malo != bueno:
+            tabla[ord(malo)] = bueno
+    return tabla
+
+
+MOJIBAKE_DOS = str.maketrans(_tabla_cp437_a_cp850())
+
+MOJIBAKE_JUGADORES = str.maketrans({
+    "\u2550": "\u00cd",
+    "\u2553": "\u00cd",
+    "\u2534": "\u00c1",
+    "\u2561": "\u00c1",
+    "\u2510": "\u00c1",
+    "\u2554": "\u00c9",
+    "\u03b1": "\u00d3",
+    "\u0398": "\u00da",
+    "\u00d0": "\u00d1",
 })
 
 
@@ -19,4 +32,4 @@ MOJIBAKE_DOS = str.maketrans({
 def texto_limpio(valor):
     if valor is None:
         return ""
-    return str(valor).translate(MOJIBAKE_DOS)
+    return str(valor).translate(MOJIBAKE_DOS).translate(MOJIBAKE_JUGADORES)
